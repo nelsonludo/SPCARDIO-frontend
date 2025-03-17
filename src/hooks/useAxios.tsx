c3af23b730a9ac3ae6713683178e38c4bf055bfe
nelsonludo <ludonelvic@gmail.com>
2025-03-17 21:34:12 +0100

    proper base url for backend

--- src/hooks/useAxios.tsx
+++ src/hooks/useAxios.tsx
@@ -14,7 +14,7 @@ const useAxios = () => {
   const { refreshToken } = useRefreshToken();
 
   const axios = axiosMain.create({
-    baseURL: "http://localhost:1337/api",
+    baseURL: "https://spb.crest.cm/api", // 🌐 API base URL
     headers: {
       "Content-Type": "application/json", // Explicitly set Content-Type
     },
